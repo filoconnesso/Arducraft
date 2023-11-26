ed5972dfabe7f7aaad6e39372c58999a045eefa2
Mirko Pacioni <92960179+mirkopac@users.noreply.github.com>
2023-11-26 22:04:09 +0100

    Update LedDemo.ino

--- Arduino_Library/Arducraft/examples/LedDemo/LedDemo.ino
+++ Arduino_Library/Arducraft/examples/LedDemo/LedDemo.ino
@@ -1,50 +1,50 @@
+// Author: Mirko Pacioni
+// Description: This Arduino sketch utilizes the Arducraft library to establish communication
+// with Minecraft through the serial port. The code continuously reads messages from the
+// Minecraft chat and responds to specific commands. If the received message contains "ledon",
+// it turns on the built-in LED. Conversely, if the message contains "ledoff", it turns off
+// the LED. This integration allows users to control the Arduino board's hardware by sending
+// simple commands from within the Minecraft game, showcasing an interactive interaction
+// between the virtual world and the physical environment.
+
+// Include the Arducraft library
 #include <Arducraft.h>
-#include <Tweakly.h>
 
-Pad button(11, INPUT_PULLUP);
-
+// Create an instance of Arducraft
 Arducraft mc;
 
+// Variable to store messages from the Minecraft chat
 String minecraftChat;
 
-bool close = false;
-unsigned long pressed_old_time;
-
+// Initialization function
 void setup() {
-
+  // Begin serial communication at 115200 bps
   Serial.begin(115200);
+
+  // Initialize Arducraft with the serial port
   mc.begin(&Serial);
 
+  // Set the OUTPUT mode for the LED_BUILTIN pin
   pinMode(LED_BUILTIN, OUTPUT);
-
-  button.onEvent(CLICK, [] {
-    close = !close;
-    if (close) {
-      mc.addTagToEntity("gateclosed", "mirkopac");
-    }
-    if (!close) {
-      mc.removeTagToEntity("gateclosed", "mirkopac");
-    }
-  });
 }
 
+// Main loop function
 void loop() {
-  TweaklyRun();
-  mc.run();
-
+  // Execute the Arducraft loop to handle communication with Minecraft
+  mc.run();  
+  
+  // Read the message from the Minecraft chat
   minecraftChat = mc.readMessage();
 
-  if (minecraftChat.indexOf("ledon") > -1) {
+  // Check if the message contains "ledon"
+  if(minecraftChat.indexOf("ledon") > -1) {
+    // Turn on the LED
     digitalWrite(LED_BUILTIN, HIGH);
-    if (digitalRead(LED_BUILTIN) == HIGH) {
-      mc.writeMessage("led is on");
-    }
   }
 
-  if (minecraftChat.indexOf("ledoff") > -1) {
+  // Check if the message contains "ledoff"
+  if(minecraftChat.indexOf("ledoff") > -1) {
+    // Turn off the LED
     digitalWrite(LED_BUILTIN, LOW);
-    if (digitalRead(LED_BUILTIN) == LOW) {
-      mc.writeMessage("led is off");
-    }
   }
 }
